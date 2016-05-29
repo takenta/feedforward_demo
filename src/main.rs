b3008c2376fa2773f62ec1cfd9e8aa2853776784
takenta <takenta0920@outlook.com>
2016-05-29 11:53:03 +0900

    Complete FeedForward

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,119 @@
 extern crate rand;
 mod tools;
 
-use std::io;
+use std::thread;
+use std::time::Duration;
 use rand::Rng;
 use tools::dual_number::DualNumber;
 
 fn main() {
-    // 入力の次元数と層のノード数
-    let num_input: usize = 5;
-    let num_node_hide: usize = 5;
-    let num_node_output: usize = 5;
-
     // 教師データ
-    let inputs: Vec<f64> = gen_rand_weights(num_input, 0.0, 1.0); // 入力
-    let teacher: &Vec<f64> = &inputs; // 期待される出力
+    let mut inputs: Vec<DualNumber> = vec![DualNumber::new(1.0),DualNumber::new(2.0),DualNumber::new(3.0),DualNumber::new(4.0),DualNumber::new(5.0),DualNumber::new(6.0),DualNumber::new(7.0),DualNumber::new(8.0),DualNumber::new(9.0)]; // 入力
+    // let mut inputs: Vec<DualNumber> = vec![DualNumber::new(1.0),DualNumber::new(2.0),DualNumber::new(3.0),DualNumber::new(4.0),DualNumber::new(5.0)]; // 入力
+    // let mut inputs: Vec<DualNumber> = vec![DualNumber::new(5.0),DualNumber::new(4.0),DualNumber::new(3.0),DualNumber::new(2.0),DualNumber::new(1.0)];
+    let teachers: Vec<DualNumber> = inputs.clone(); // 期待される出力
+
+    // 入力の次元数と層のノード数
+    let num_input: usize = inputs.len();
+    let num_node_hide: usize = 10;
+    let num_node_output: usize = num_input;
 
     // ネットワークの生成
     // 重み
-    let weights_hide: Vec<f64> = gen_rand_weights(num_input*num_node_hide, 0.0, 1.0);
-    let weights_output: Vec<f64> = gen_rand_weights(num_input*num_node_output, 0.0, 1.0);
+    let weights_hide_1: Vec<f64> = gen_rand_weights(num_input*num_node_hide, 0.0, 0.10);
+    let weights_hide_2: Vec<f64> = gen_rand_weights(num_node_hide*num_node_hide, 0.0, 0.10);
+    let weights_output: Vec<f64> = gen_rand_weights(num_node_hide*num_node_output, 0.0, 0.10);
 
     // 活性化関数
-    let func_hide: Box<Fn(f64) -> f64> = Box::new(move |x: f64| x.max(0.0)); // 標準正規化関数
-    let func_output: Box<Fn(f64) -> f64> = Box::new(move |x: f64| x); // 恒等写像
+    // let func_hide_sig: Box<Fn(DualNumber) -> DualNumber> = Box::new(move |x: DualNumber| x.max(DualNumber::new(0.0))); // 標準正規化関数
+    let func_hide_rel: Box<Fn(DualNumber) -> DualNumber> = Box::new(move |x: DualNumber| (DualNumber::new(1.0) - (-x).exp()).recip()); // 標準シグモイド関数
+    let func_output: Box<Fn(DualNumber) -> DualNumber> = Box::new(move |x: DualNumber| x); // 恒等写像
 
     // レイヤーの生成
-    let layer_hide: FeedForward = FeedForward::new(weights_hide, inputs.len(), 0.0, &func_hide); // 隠れ層
-    let mut layer_output: FeedForward = FeedForward::new(weights_output, inputs.len(), 0.0, &func_output); // 出力層
+    let mut layer_hide_1: FeedForward = FeedForward::new(weights_hide_1, num_input, gen_rand_weights(num_node_hide, 1.0, 1.0), &func_hide_rel); // 隠れ層
+    let mut layer_hide_2: FeedForward = FeedForward::new(weights_hide_2, num_node_hide, gen_rand_weights(num_node_hide, 1.0, 1.0), &func_hide_rel); // 隠れ層
+    let mut layer_output: FeedForward = FeedForward::new(weights_output, num_node_hide, gen_rand_weights(num_node_output, 1.0, 1.0), &func_output); // 出力層
 
     // 誤差関数（二乗誤差；回帰・入力が連続したデータである時に有効）
-    let error_func: Box<Fn(&Vec<f64>, &Vec<f64>) -> f64> = Box::new(move |outputs: &Vec<f64>, teachers: &Vec<f64>| {
-        outputs.iter().zip(teachers.iter()).fold(0.01, |acc: f64, (&output, &teacher)| {
-            acc + (output - teacher).abs().powf(2.0)
-        }) * 0.5
-    });
+    let error_func: Box<Fn(&DualNumber, &DualNumber) -> DualNumber> = Box::new(move |&output, &teacher| (output-teacher).powf(2.0) * DualNumber::new(0.5));
+
+    let mut count = 0;
 
     'main: loop {
         println!("");
 
-        println!("===============");
+        count += 1;
+        println!("====== {:4} ======", &count);
 
+        // 入力層を出力
         println!("input:");
-        for input in &inputs {
-            print!(" {:.*}", 5, (input * 1000.0).round() * 0.001);
+        for elem in &inputs {
+            print!(" {:.*}", 5, elem.x);
         }
         println!("\n");
 
+        // 隠れ層を出力
         println!("weights hide:");
-        layer_hide.print_weights(&inputs.len());
+        layer_hide_1.print_weights();
         println!("");
 
-        println!("weights output:");
-        layer_output.print_weights(&inputs.len());
+        // 隠れ層を出力
+        println!("weights hide:");
+        layer_hide_2.print_weights();
         println!("");
+
+        // 出力層を出力
+        println!("weights output:");
+        layer_output.print_weights();
+        println!("");
+
+        for input in &mut inputs {
+            input.select()
+        }
 
         // レイヤー２枚で演算
         println!("result:");
-        let outputs: Vec<f64> = layer_output.outputs(&layer_hide.outputs(&inputs));
-        for output in &outputs {
-            print!(" {:.*}", 5, (output * 1000.0).round() * 0.001);
-        };
-        println!("\n");
-
-        println!("answer:");
-        for answer in teacher {
-            print!(" {:.*}", 5, (answer * 1000.0).round() * 0.001);
+        let outputs_hide_1: Vec<DualNumber> = layer_hide_1.outputs(&inputs);
+        let outputs_hide_2: Vec<DualNumber> = layer_hide_2.outputs(&outputs_hide_1);
+        let outputs: Vec<DualNumber> = layer_output.outputs(&outputs_hide_2);
+        for elem in &outputs {
+            print!(" {:.*}", 5, elem.x);
         }
         println!("\n");
 
-        println!("diff:\n {:.*}", 5, (calc_error(&outputs, &teacher, &error_func) * 1000.0).round() * 0.001);
+        // 教師データの表示
+        println!("teachers:");
+        for elem in &teachers {
+            print!(" {:.*}", 5, elem.x);
+        }
+        println!("\n");
+
+        // 誤差の算出
+        let errors_output = layer_output.calc_error_output(&outputs, &teachers, &error_func);
+        let errors_hide_2 = layer_hide_2.calc_error_hide(&outputs_hide_2, &layer_output, &errors_output);
+        let errors_hide_1 = layer_hide_1.calc_error_hide(&outputs_hide_1, &layer_hide_2, &errors_hide_2);
+
+        // 出力層の誤差の表示
+        let loss = outputs.iter().zip(teachers.iter()).fold(DualNumber::new(0.0), |acc, (o, t)| {
+            acc + error_func(o, t)
+        });
+        println!("loss:\n {:?}", &loss.x);
         println!("");
 
-        println!("Please input modification for output layer (ex: 0 0 0.01) (EXIT: halt):");
+        if loss.x < 0.001 { break 'main; }
 
-        let mut input_string: String = String::new();
-        io::stdin().read_line(&mut input_string).expect("Failed to read line");
+        // 重みの更新
+        let rate: f64 = 0.15;
+        layer_output.update_weights(&errors_output, rate);
+        layer_output.update_bias(&errors_output, rate);
+        layer_hide_2.update_weights(&errors_hide_2, rate);
+        layer_hide_2.update_bias(&errors_hide_2, rate);
+        layer_hide_1.update_weights(&errors_hide_1, rate);
+        layer_hide_1.update_bias(&errors_hide_1, rate);
 
-        if input_string.trim() == "halt" { break 'main; }
+        thread::sleep(Duration::from_millis(100));
 
-        let modifications: Vec<&str> = input_string.split_whitespace().collect();
-        // let target: &str = modifications[0].trim();
-        let target_row: usize = modifications[0].trim().parse().expect("Please input a number!");
-        let target_column: usize = modifications[1].trim().parse().expect("Please input a number!");
-        let new_weight: f64 = modifications[2].trim().parse().expect("Please input a number!");
-
-        layer_output.update_weights(target_row, target_column, new_weight);
+        if count >= 10000 { break 'main; }
     }
 }
 
@@ -104,25 +133,15 @@ pub fn gen_rand_weights(num: usize, low: f64, high: f64) -> Vec<f64> {
     rng.gen_iter::<f64>().take(num).map(|item| (high - low)*item + low).collect::<Vec<f64>>()
 }
 
-pub fn calc_error<'a>(output: &'a Vec<f64>, teacher: &'a Vec<f64>, func: &'a Box<Fn(&Vec<f64>, &Vec<f64>) -> f64>) -> f64 {
-    func(output, teacher)
-}
-
 /// 行列を成型して表示する。
 pub fn print_matrix<'a>(matrix: &'a Vec<f64>, length: &'a usize) {
     for line in matrix.chunks(*length) {
         for item in line {
-            println!(" {:.*}", 2, item);
+            print!(" {:.*}", 2, item);
         }
         println!("");
     }
 }
-
-/// 関数を表すクロージャを受け取って、その関数の数値微分を表すクロージャを返す。
-pub fn diff_func(func: Box<Fn(DualNumber) -> DualNumber>) -> Box<Fn(f64) -> f64> {
-    Box::new(move |x: f64| func(DualNumber::new(x)).dx)
-}
-
 
 // ========================================================================= //
 
@@ -130,12 +149,12 @@ pub fn diff_func(func: Box<Fn(DualNumber) -> DualNumber>) -> Box<Fn(f64) -> f64>
 pub struct FeedForward<'a> {
     weights: Vec<f64>,
     len_row: usize,
-    bias: f64,
-    func: &'a Box<Fn(f64) -> f64>,
+    bias: Vec<f64>,
+    func: &'a Box<Fn(DualNumber) -> DualNumber>,
 }
 
 impl<'a> FeedForward<'a> {
-    pub fn new(weights: Vec<f64>, len_row: usize, bias: f64, func: &'a Box<Fn(f64) -> f64>) -> FeedForward<'a> {
+    pub fn new(weights: Vec<f64>, len_row: usize, bias: Vec<f64>, func: &'a Box<Fn(DualNumber) -> DualNumber>) -> FeedForward<'a> {
         FeedForward {
             weights: weights,
             len_row: len_row,
@@ -145,33 +164,64 @@ impl<'a> FeedForward<'a> {
     }
 
     /// 層の出力を算出する。
-    pub fn outputs(&self, inputs: &'a Vec<f64>) -> Vec<f64> {
-        self.calc_inputs(inputs).iter().map(|&elem| (self.func)(elem)).collect::<Vec<f64>>()
+    pub fn outputs(&self, inputs: &'a Vec<DualNumber>) -> Vec<DualNumber> {
+        self.calc_inputs(inputs).iter().map(|&elem| {
+            (self.func)(elem)
+        }).collect::<Vec<DualNumber>>()
     }
 
     /// 重み付き総入力を算出する。
-    fn calc_inputs(&self, inputs: &'a Vec<f64>) -> Vec<f64> {
+    fn calc_inputs(&self, inputs: &'a Vec<DualNumber>) -> Vec<DualNumber> {
         self.weights.chunks(inputs.len()).map(|line_weights| {
-            line_weights.iter().zip(inputs.iter()).fold(0.0, |acc: f64, (&weight, &input)| {
-                acc + weight * input
-            }) + self.bias
-        }).collect::<Vec<f64>>()
+            line_weights.iter().zip(inputs.iter()).fold(DualNumber::new(0.0), |acc: DualNumber, (&weight, &input)| {
+                acc + DualNumber::new(weight) * input
+            })
+        }).zip(self.bias.iter()).map(|(elem, &b)| elem + DualNumber::new(b)).collect::<Vec<DualNumber>>()
+    }
+
+    /// 出力層の誤差を算出する。
+    fn calc_error_output<'b>(&self, outputs: &'b Vec<DualNumber>, teachers: &'b Vec<DualNumber>, func_error: &'b Box<Fn(&DualNumber, &DualNumber) -> DualNumber>) -> Vec<f64> {
+        let mut selected = outputs.clone();
+        for elem in &mut selected {
+            elem.dx = 1.0;
+        }
+
+        let vec_error = teachers.iter().zip(selected.iter()).map(|(t, o)| {
+            func_error(&o, &t).dx
+        }).collect::<Vec<f64>>();
+
+        let vec_output = outputs.iter().map(|&elem| {
+            elem.dx
+        }).collect::<Vec<f64>>();
+
+        calc_hadamard(vec_error, vec_output)
+    }
+
+    /// 隠れ層の誤差を算出する。
+    pub fn calc_error_hide<'b>(&mut self, outputs: &'b Vec<DualNumber>, prev_layer: &'b FeedForward, prev_errors: &'b Vec<f64>) -> Vec<f64> {
+        let transposed = prev_layer.transposed_weights();
+
+        let vec_error = transposed.chunks(prev_errors.len()).map(|line_weights| {
+            line_weights.iter().zip(prev_errors.iter()).fold(0.0, |acc: f64, (&weight, &error)| {
+                acc + weight * error
+            })
+        }).collect::<Vec<f64>>();
+
+        let vec_output = outputs.iter().map(|&elem| {
+            elem.dx
+        }).collect::<Vec<f64>>();
+
+        calc_hadamard(vec_error, vec_output)
     }
 
     /// 重み行列を成型して表示する
-    pub fn print_weights(&'a self, length: &'a usize) {
-        for line_weights in self.weights.chunks(*length) {
+    pub fn print_weights(&self) {
+        for line_weights in self.weights.chunks(self.len_row) {
             for weight in line_weights {
-                print!(" {:.*}", 2, weight);
+                print!(" {:.*}", 5, weight);
             }
             println!("");
         }
-    }
-
-    /// 行と列を指定して、その箇所の重みを更新する。
-    pub fn update_weights(&mut self, row: usize, column: usize, new_weight: f64) {
-        let target = row * self.len_row + column;
-        self.weights[target] = new_weight;
     }
 
     /// 重み行列を転置する。
@@ -191,32 +241,22 @@ impl<'a> FeedForward<'a> {
 
         for row in 0..num_row {
             for column in row..self.len_row {
-                transposed.swap(row * self.len_row + column, column * self.len_row + row)
+                transposed.swap(row * self.len_row + column, column * num_row + row)
             }
         }
 
         transposed
     }
 
-    // pub fn calc_output_error(&self, inputs: &'a Vec<f64>, teachers: &'a Vec<f64>, func_error: &'a Box<Fn(&DualNumber, &DualNumber) -> DualNumber>) -> &'a Vec<f64> {
+    pub fn update_weights<'b>(&mut self, errors: &'b Vec<f64>, rate: f64) {
+        for i in 0..self.weights.len() {
+            self.weights[i] -= rate * errors[i/self.len_row]
+        }
+    }
 
-    //     // let diff_func_error = teachers.iter().map(|&t| {
-    //     //     diff_func(Box::new(move |x| func_error(&t, &x)))
-    //     // }).collect::<Vec<Fn(f64) -> f64>>();
-    //     let diff_func_output = diff_func(*self.func);
-
-
-    //     let vec_error = teachers.iter().zip(inputs.iter()).map(|&t, &i| {
-    //         let dn_teacher: DualNumber = DualNumber::new(t);
-    //         let dn_input: DualNumber = DualNumber::new(i);
-
-    //         func_error(dn_teacher, dn_input).dx
-    //     }).collect::<Vec<f64>>();
-    //     let vec_output = inputs.iter().map(|&i| {
-    //         let dn_input: DualNumber = DualNumber::new(i);
-
-    //         self.func()
-    //     }).collect::<Vec<f64>>();
-    //     &calc_hadamard(vec_error, vec_output)
-    // }
+    pub fn update_bias<'b>(&mut self, errors: &'b Vec<f64>, rate: f64) {
+        for i in 0..self.bias.len() {
+            self.bias[i] -= rate * errors[i]
+        }
+    }
 }
--- src/tools/dual_number.rs
+++ src/tools/dual_number.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add,Div,Mul,Sub};
+use std::ops::{Add,Div,Mul,Sub,Neg};
+use std::fmt;
 
 #[derive(Debug,Copy,Clone)]
 pub struct DualNumber {
@@ -15,52 +16,127 @@ impl DualNumber {
     }
 
     pub fn select(&mut self) {
-        self.dx = 1.0;
+        if self.dx == 0.0 {
+            self.dx = 1.0
+        }
     }
 
     pub fn exclude(&mut self) {
-        self.dx = 0.0;
+        self.dx = 0.0
     }
 
-    pub fn sqrt(&self) -> DualNumber {
+    pub fn recip(self) -> DualNumber {
+        DualNumber {
+            x: self.x.recip(),
+            dx: - self.dx/(self.x * self.x)
+        }
+    }
+
+    pub fn sqrt(self) -> DualNumber {
         DualNumber {
             x: self.x.sqrt(),
             dx: 0.5*self.dx/self.x.sqrt(),
         }
     }
 
-    pub fn exp(&self) -> DualNumber {
+    pub fn exp(self) -> DualNumber {
         DualNumber {
             x: self.x.exp(),
             dx: self.dx*self.x.exp(),
         }
     }
 
-    pub fn ln(&self) -> DualNumber {
+    pub fn ln(self) -> DualNumber {
         DualNumber {
             x: self.x.ln(),
             dx: self.dx/self.x,
         }
     }
 
-    pub fn log(&self, base: f64) -> DualNumber {
+    pub fn log(self, base: f64) -> DualNumber {
         DualNumber {
             x: self.x.log(base),
             dx: self.dx/self.x,
         }
     }
 
-    pub fn sin(&self) -> DualNumber {
+    pub fn sin(self) -> DualNumber {
         DualNumber {
             x: self.x.sin(),
             dx: self.dx.cos(),
         }
     }
 
-    pub fn cos(&self) -> DualNumber {
+    pub fn cos(self) -> DualNumber {
         DualNumber {
             x: self.x.cos(),
             dx: -(self.dx.sin()),
+        }
+    }
+
+    pub fn max(self, other: DualNumber) -> DualNumber {
+        if self.x > other.x {
+            self
+        } else {
+            other
+        }
+    }
+
+    pub fn min(self, other: DualNumber) -> DualNumber {
+        if self.x < other.x {
+            self
+        } else {
+            other
+        }
+    }
+
+    pub fn abs(self) -> DualNumber {
+        DualNumber {
+            x: self.x.abs(),
+            dx: 0.0,
+        }
+    }
+
+    pub fn round(self) -> DualNumber {
+        DualNumber {
+            x: self.x.round(),
+            dx: self.dx,
+        }
+    }
+
+    pub fn powi(self, n: i32) -> DualNumber {
+        if self.x == 0.0 {
+            DualNumber {
+                x: 0.0,
+                dx: 0.0,
+            }
+        } else if self.x < 0.0 {
+            let n_f64 = f64::from(n);
+            if (n % 2) == 0 {
+                (DualNumber::new(n_f64) * (-self).ln()).exp()
+            } else {
+                -((DualNumber::new(n_f64) * (-self).ln()).exp())
+            }
+        } else {
+            let n_f64 = f64::from(n);
+            (DualNumber::new(n_f64) * self.ln()).exp()
+        }
+    }
+
+    pub fn powf(self, n: f64) -> DualNumber {
+        if self.x == 0.0 {
+            DualNumber {
+                x: 0.0,
+                dx: 0.0,
+            }
+        } else if self.x < 0.0 {
+            if (n % 2.0) == 0.0 {
+                (DualNumber::new(n) * (-self).ln()).exp()
+            } else {
+                -((DualNumber::new(n) * (-self).ln()).exp())
+            }
+        } else {
+            (DualNumber::new(n) * self.ln()).exp()
         }
     }
 }
@@ -106,5 +182,22 @@ impl Sub<DualNumber> for DualNumber {
             x: self.x - other.x,
             dx: self.dx - other.dx,
         }
+    }
+}
+
+impl Neg for DualNumber {
+    type Output = DualNumber;
+
+    fn neg(self) -> DualNumber {
+        DualNumber {
+            x: -self.x,
+            dx: -self.dx,
+        }
+    }
+}
+
+impl fmt::Display for DualNumber {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.dx)
     }
 }
